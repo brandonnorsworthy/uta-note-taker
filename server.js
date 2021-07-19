481dae7190cf7f798abffd8306058f719f3387f3
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-19 18:38:29 -0500

    added comments and console logs for server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080; //express syntax / heroku
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -15,20 +15,28 @@ app.get("/api/notes", (req, res) => {
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) throw err;
 
-    res.send(JSON.parse(data));
+    res.send(JSON.parse(data)); //send back the new json
   })
 })
 
+app.get('/notes', (req, res) => {
+  console.log(`User is requesting the notes html file`);
+
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+}); //gets the notes.html files
+
+//when user asks for the notes file
 app.post("/api/notes", (req, res) => {
+  console.log(`User creating new a new note`);
+
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) throw err;
 
-    let totalNotes = JSON.parse(data);
-    let newNote = req.body;
-    newNote.id = uuidv4();
-    console.log(newNote);
-    totalNotes.push(newNote);
-    fs.writeFile("db/db.json", JSON.stringify(totalNotes), (err) => {
+    let totalNotes = JSON.parse(data); // parse the json object to get ready to add to it
+    let newNote = req.body; //go into the req and look at the payload
+    newNote.id = uuidv4(); //generate new note id
+    totalNotes.push(newNote); //push new note into object
+    fs.writeFile("db/db.json", JSON.stringify(totalNotes), (err) => { //update the db json file with new content
         if(err) throw err;
     })
 
@@ -36,11 +44,14 @@ app.post("/api/notes", (req, res) => {
   })
 })
 
+//user clicks delete button function grabs the id sent in payload and finds it in the json file
 app.delete("/api/notes/:id", (req, res) => {
+
+  console.log(`Deleting based on ${req.params.id} from user`);
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) throw err;
 
-    let totalNotes = JSON.parse(data);
+    let totalNotes = JSON.parse(data); // parse the json object to get ready to add to it
 
     totalNotes.splice(req.params.id, 1);
     fs.writeFile("db/db.json", JSON.stringify(totalNotes), (err) => {
@@ -51,8 +62,12 @@ app.delete("/api/notes/:id", (req, res) => {
   })
 })
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+//default to main index welcome screen
+app.get('*', (req, res) => {
+  console.log("Redirecting user to index, requested out of index/notes")
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
 
 app.listen(PORT, () =>
 {
