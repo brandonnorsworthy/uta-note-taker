dc86fc178035553521aec737065ffc1b58cbdc26
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-21 14:50:00 -0500

    heroku port enviorment added

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require('uuid');
 const db = require('./db/db.json');
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 app.use(express.json());
